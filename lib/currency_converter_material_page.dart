ed616fd353bf303920368dd225b890c28e435a5e
soumya-99 <soumyadeepmondal1234@gmail.com>
2023-12-13 15:15:20 +0530

    padding and containers

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -5,6 +5,16 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const outlineBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.vertical(
+        top: Radius.circular(20),
+      ),
+    );
+
     return const Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Column(
@@ -18,35 +28,32 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
               color: Color.fromARGB(255, 255, 255, 255),
             ),
           ),
-          TextField(
-            style: TextStyle(
-              color: Color.fromARGB(255, 0, 0, 0),
-            ),
-            decoration: InputDecoration(
-              // labelText: "Please enter the amount in INR",
-              // helperText: "Hello",
-              hintText: "Please enter the amount in INR",
-              hintStyle: TextStyle(
-                color: Colors.black,
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              style: TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
               ),
-              prefixIcon: Icon(
-                Icons.monetization_on_outlined,
-                // color: Colors.white60,
-              ),
-              prefixIconColor: Colors.black,
-              filled: true,
-              fillColor: Colors.white,
-              border: InputBorder.none,
-              // focusedBorder: InputBorder.none,
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  width: 2.0,
-                  style: BorderStyle.solid,
+              decoration: InputDecoration(
+                // labelText: "Please enter the amount in INR",
+                // helperText: "Hello",
+                hintText: "Please enter the amount in INR",
+                hintStyle: TextStyle(
+                  color: Colors.black,
                 ),
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(20),
+                prefixIcon: Icon(
+                  Icons.monetization_on_outlined,
+                  // color: Colors.white60,
                 ),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                border: InputBorder.none,
+                // focusedBorder: InputBorder.none,
+                focusedBorder: outlineBorder,
+                enabledBorder: outlineBorder,
               ),
+              keyboardType: TextInputType.number,
             ),
           ),
         ],
